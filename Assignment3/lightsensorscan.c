64109b83c5c1edc976dd34614d9d6b14f3109010
Alexander Lee Hayes <hayesall@silo.soic.indiana.edu>
2016-02-05 11:20:40 -0500

    Added a function to translate between potentiometer values and degrees. Will be used for testing in Part 2.

--- Assignment3/lightsensorscan.c
+++ Assignment3/lightsensorscan.c
@@ -2,6 +2,10 @@
 #pragma config(Motor,  port5,  Servo,          tmotorServoContinuousRotation, openLoop)
 #pragma config(Sensor, in1,    POT,            sensorPotentiometer)
 
+int PtoD(int value) {
+	return ((0.07 * value) + 11.13);
+}
+
 task main()
 {
 
@@ -9,7 +13,7 @@ while(true) {
 
 int x,y,ptsens; // x corresponds to the light sensor, y is the servo position corresopnding to the highest value of x, ptsens is the Potentiometer
 x = SensorValue[LightIn]; // take an initial reading of the Light, this will be comared in the for loop
-ptsens = SensorValue[POT];// set ptsens to the value of the Potentiometer
+//ptsens = SensorValue[POT];// set ptsens to the value of the Potentiometer
 
 //writeDebugStreamLine("%d",x); //print the current position of the Servo, mostly for visually representing what is happening in real life.
 writeDebugStreamLine("%d",SensorValue[POT]); //print value of the potentiometer.
@@ -20,18 +24,19 @@ wait1Msec(1000); // wait a second (this is important when the "eye" is reseting)
 for(int i= -127; i<127; i=i+4) { // start at -127, increments by 4, "stops at 127" (technically 125)
 
 	motor[Servo] = i; // set Servo to the value of i (-127, -123, -119, ... 121, 125)
-	wait1Msec(150); // wait a bit in between the values (for accurate readings)
+	wait1Msec(100); // wait a bit in between the values (for accurate readings)
 	//writeDebugStreamLine("The current position is %d", i);
-	writeDebugStreamLine("potentiometer value is %d", SensorValue[POT]); // print the potentiometer values
+	//writeDebugStreamLine("potentiometer value is %d", SensorValue[POT]); // print the potentiometer values
 	if (x >= SensorValue[LightIn]) { // compare the initial reading to the
 		x = SensorValue[LightIn];
+		ptsens = SensorValue[POT];
 		y = i;
-		wait1Msec(150);
+		//wait1Msec(150);
 	}
 }
 
 motor[Servo] = y;
-writeDebugStreamLine("Now facing brightest position");
+writeDebugStreamLine("Now facing brightest position, potentiometer value is %d, the angle is: %d",ptsens,PtoD(ptsens));
 wait1Msec(3000);
 
 }
