3815c139b5653dd57e8c8865fc44196de73badc3
TheVanishingMan <alexander@batflyer.net>
2016-02-11 11:02:47 -0500

    Added drivebylight.c the file for part 3 of assignment 3.  There are still a few bugs to work out: most notably the issue with the bump sensors in the wander funcion, and the program is currently ignoring the scanner function.

--- Assignment3/lightsensorscan.c
+++ Assignment3/lightsensorscan.c
@@ -9,36 +9,36 @@ int PtoD(int value) {
 task main()
 {
 
-while(true) {
-
-int x,y,ptsens; // x corresponds to the light sensor, y is the servo position corresopnding to the highest value of x, ptsens is the Potentiometer
-x = SensorValue[LightIn]; // take an initial reading of the Light, this will be comared in the for loop
-//ptsens = SensorValue[POT];// set ptsens to the value of the Potentiometer
-
-//writeDebugStreamLine("%d",x); //print the current position of the Servo, mostly for visually representing what is happening in real life.
-writeDebugStreamLine("%d",SensorValue[POT]); //print value of the potentiometer.
-y = -127; // set the initial y value to -127 (minimum), later the for loop will increment this value by 4 each time
-motor[Servo] = y; // set the motor position to be y
-wait1Msec(1000); // wait a second (this is important when the "eye" is reseting)
-
-for(int i= -127; i<127; i=i+4) { // start at -127, increments by 4, "stops at 127" (technically 125)
-
-	motor[Servo] = i; // set Servo to the value of i (-127, -123, -119, ... 121, 125)
-	wait1Msec(100); // wait a bit in between the values (for accurate readings)
+  while(true) {
+    
+    int x,y,ptsens; // x corresponds to the light sensor, y is the servo position corresopnding to the highest value of x, ptsens is the Potentiometer
+    x = SensorValue[LightIn]; // take an initial reading of the Light, this will be comared in the for loop
+    //ptsens = SensorValue[POT];// set ptsens to the value of the Potentiometer
+    
+    //writeDebugStreamLine("%d",x); //print the current position of the Servo, mostly for visually representing what is happening in real life.
+    writeDebugStreamLine("%d",SensorValue[POT]); //print value of the potentiometer.
+    y = -127; // set the initial y value to -127 (minimum), later the for loop will increment this value by 4 each time
+    motor[Servo] = y; // set the motor position to be y
+    wait1Msec(1000); // wait a second (this is important when the "eye" is reseting)
+    
+    for(int i= -127; i<127; i=i+4) { // start at -127, increments by 4, "stops at 127" (technically 125)
+      
+      motor[Servo] = i; // set Servo to the value of i (-127, -123, -119, ... 121, 125)
+      wait1Msec(100); // wait a bit in between the values (for accurate readings)
 	//writeDebugStreamLine("The current position is %d", i);
 	//writeDebugStreamLine("potentiometer value is %d", SensorValue[POT]); // print the potentiometer values
-	if (x >= SensorValue[LightIn]) { // compare the initial reading to the
-		x = SensorValue[LightIn];
-		ptsens = SensorValue[POT];
+      if (x >= SensorValue[LightIn]) { // compare the initial reading to the
+	x = SensorValue[LightIn];
+	ptsens = SensorValue[POT];
 		y = i;
 		//wait1Msec(150);
-	}
-}
-
-motor[Servo] = y;
-writeDebugStreamLine("Now facing brightest position, potentiometer value is %d, the angle is: %d",ptsens,PtoD(ptsens));
-wait1Msec(14000);
-
-}
-
+      }
+    }
+    
+    motor[Servo] = y;
+    writeDebugStreamLine("Now facing brightest position, potentiometer value is %d, the angle is: %d",ptsens,PtoD(ptsens));
+    wait1Msec(14000);
+    
+  }
+  
 }
